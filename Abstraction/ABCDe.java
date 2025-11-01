f30dcca8a9943c2b29979de26959b944c6853f86
Bhagyashri Shinde <bhagyashrishinde9700@gmail.com>
2025-11-01 18:08:36 +0530

    core java program

--- Abstraction/ABCDe.java
+++ Abstraction/ABCDe.java
@@ -11,11 +11,12 @@ public class ABCDe {
 class XYZ extends ABCDe {
 
     public void m1() {  // Must also be public
-        System.out.println("This is m1 method from XYZ (Child Class)");
+        System.out.println("This is m1 method from ABCDe (Parent Class)");
     }
 
     public static void main(String[] args) {
-        XYZ x1 = new XYZ();
-        x1.m1();  // Calls overridden method in XYZ
+
+       // XYZ x1 = new XYZ();
+       // x1.m1();  // Calls overridden method in XYZ
     }
 }
